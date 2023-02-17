61d3df5ecad43ee932cc2c23f174eb6d1e4bf54a
Alexander Mitropolskiy <AAMitropolskiy@sberbank.ru>
2023-02-17 16:14:15 +0300

    Test publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     kotlin("jvm") version "1.8.10"
     id("io.gitlab.arturbosch.detekt") version "1.22.0"
+    id("maven-publish")
 }
 
 group = "aam"
@@ -20,4 +21,18 @@ tasks.test {
 
 kotlin {
     jvmToolchain(11)
+}
+
+publishing {
+    publications {
+        register<MavenPublication>("release") {
+            groupId = "com.github.octa-one"
+            artifactId = "kmold"
+            version = "0.1"
+
+            afterEvaluate {
+                from(components["release"])
+            }
+        }
+    }
 }
